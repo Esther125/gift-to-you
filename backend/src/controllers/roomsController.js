51b67ac1957efad62782c34d816ae3f9878e07c7
yun-si <110306079@g.nccu.edu.tw>
2024-11-09 20:23:46 +0800

    feat: Add redis client and api to test

--- backend/src/controllers/roomsController.js
+++ backend/src/controllers/roomsController.js
@@ -1,3 +1,5 @@
+import RedisClient from '../clients/redisClient.js';
+
 class RoomsController {
     async createRoom(req, res) {
         console.log('----RoomsController.createRoom');
@@ -10,6 +12,25 @@ class RoomsController {
         // TODO: 實現加入房間邏輯
         res.status(200).json({ message: 'Join room logic not implemented yet' });
     }
+
+    _redisTest = async (req, res) => {
+        console.log('----RoomsController redisTest');
+
+        try {
+            const redis = new RedisClient();
+
+            await redis._connect();
+            await redis._set('key', 'RedisTest');
+            const value = await redis._get('key');
+            console.log('Redis return value: ' + value);
+            await redis._setExpire('key', 10);
+            redis._quit();
+            res.status(200).json({ message: value });
+        } catch {
+            console.log('Redis Error');
+            res.status(500);
+        }
+    };
 }
 
 export default RoomsController;
--- backend/src/routes/roomsRouter.js
+++ backend/src/routes/roomsRouter.js
@@ -6,5 +6,6 @@ const roomsController = new RoomsController();
 
 roomsRouter.post('/rooms', roomsController.createRoom);
 roomsRouter.post('/rooms/:roomId/join', roomsController.joinRoom);
+roomsRouter.get('/rooms/redisTest', roomsController._redisTest);
 
 export default roomsRouter;
