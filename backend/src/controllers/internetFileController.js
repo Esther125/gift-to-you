c822c52f7fa27c9a3137be1515da97ebc6076f85
Esther125 <friday50523@gmail.com>
2024-11-18 10:23:34 +0800

    fix: fix error handling when no file upload

--- backend/src/controllers/internetFileController.js
+++ backend/src/controllers/internetFileController.js
@@ -3,21 +3,20 @@ import path from 'path';
 
 class InternetFileController {
     constructor() {
-        this.fileService = new InternetFileService();
+        this.internetFileService = new InternetFileService();
         this.upload = this.upload.bind(this);
         this.download = this.download.bind(this);
     }
 
     async upload(req, res) {
         console.log('----InternetFileController.upload');
-        // 實現上傳檔案邏輯
         try {
-            const filename = await this.fileService.upload(req, res);
+            const filename = await this.internetFileService.upload(req, res);
             const fileId = path.basename(filename, path.extname(filename));
-            res.json({ message: 'File successfully uploaded', fileId: fileId });
+            res.status(200).json({ message: 'File uploaded successfully.', fileId: fileId });
         } catch (error) {
-            console.error(error);
-            res.status(500).json({ message: 'Error uploading file', error: error.message });
+            console.error('Error uploading file: ', error);
+            res.status(500).json({ message: 'Failed to upload the file.', error: error.message });
         }
     }
 
--- backend/src/services/internetFileService.js
+++ backend/src/services/internetFileService.js
@@ -12,7 +12,7 @@ class InternetFileService {
     async upload(req, res) {
         // 實現上傳檔案邏輯
         if (!req.files || Object.keys(req.files).length === 0) {
-            return res.status(400).send('No files were uploaded.');
+            throw new Error('No files were uploaded.');
         }
 
         const uploadFile = req.files.uploadFile;
@@ -24,11 +24,11 @@ class InternetFileService {
         const filename = this._generateUniqueFilename(uploadFile.name);
         const uploadPath = path.join(rootPath, '/uploads/', filename);
 
-        // 把檔案存到指定路徑下
+        // 把檔案存到指定路徑下並回傳結果
         return new Promise((resolve, reject) => {
             uploadFile.mv(uploadPath, function (err) {
                 if (err) {
-                    reject(err);
+                    reject(new Error(err));
                 } else {
                     resolve(filename);
                 }
