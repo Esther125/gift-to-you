c4e4dab20989981bc1c94abdb27fa93145f07bc3
amyhsu <amy991225@gmail.com>
2024-11-24 08:49:01 +0800

    CNS-33 feat: add register and login in authService

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,15 +1,73 @@
+import AuthService from '../services/authService.js';
 class AuthController {
-    async register(req, res) {
-        console.log('----AuthController.register');
-        // TODO: 實現註冊邏輯
-        res.status(201).json({ message: 'User registered logic not implemented yet' });
+    constructor() {
+        this.authService = new AuthService();
     }
 
-    async login(req, res) {
-        console.log('----AuthController.login');
-        // TODO: 實現登錄邏輯
-        res.status(200).json({ message: 'Login logic not implemented yet' });
-    }
+    register = async (req, res) => {
+        console.log('[AuthController] -----register-----');
+        let email, password, userName;
+        try {
+            email = req.body.email;
+            if (!email) {
+                res.status(400).json({ message: 'Email is required' });
+                return;
+            }
+            password = req.body.password;
+            if (!password) {
+                res.status(400).json({ message: 'Password is required' });
+                return;
+            }
+            userName = req.body.userName;
+            if (!userName) {
+                res.status(400).json({ message: 'UserName is required' });
+                return;
+            }
+            const userInfo = await this.authService.register(email, password, userName);
+
+            if (userInfo.create) {
+                res.status(201).json({ message: 'Register success', data: userInfo.data });
+            } else {
+                res.status(200).json({ message: 'User already registered', data: userInfo.data });
+            }
+        } catch {
+            console.error(`[AuthController] error when ${email} try to register`);
+            res.status(500).json({ message: `Error when ${email} try to register` });
+        }
+    };
+
+    login = async (req, res) => {
+        console.log('[AuthController] -----login-----');
+        let email, password;
+        try {
+            email = req.body.email;
+            if (!email) {
+                res.status(400).json({ message: 'Email is required' });
+                return;
+            }
+            password = req.body.password;
+            if (!password) {
+                res.status(400).json({ message: 'Password is required' });
+                return;
+            }
+
+            const loginResult = await this.authService.login(email, password);
+            if (loginResult.success) {
+                res.status(200).json({ message: 'Login success', data: loginResult.data });
+            } else {
+                res.status(401).json({ message: loginResult.error });
+            }
+        } catch {
+            console.error(`[AuthController] error when ${email} try to login`);
+            res.status(500).json({ message: `Error when ${email} try to login` });
+        }
+    };
+
+    logout = async (req, res) => {
+        console.log('[AuthController] -----logout-----');
+        // TODO: 實現登出邏輯
+        res.status(200).json({ message: 'Logout logic not implemented yet' });
+    };
 }
 
 export default AuthController;
---
+++ backend/src/services/authService.js
@@ -0,0 +1,93 @@
+import { userInfo } from 'os';
+import DynamodbService from './dynamodbService.js';
+import crypto from 'crypto';
+
+class AuthService {
+    constructor() {
+        this._dynamodbService = new DynamodbService();
+        this._USERID_LENGTH = 8;
+    }
+
+    _genNewUserID = async () => {
+        let userID;
+        let isUserIDExisted;
+        do {
+            userID = crypto.randomBytes(this._USERID_LENGTH / 2).toString('hex');
+            isUserIDExisted = await this._dynamodbService.checkUserIDExist(userID);
+        } while (isUserIDExisted);
+        return userID;
+    };
+
+    _hashPassword = (password, passwordSalt) => {
+        // hash password
+        return crypto.pbkdf2Sync(password, passwordSalt, 1000, 64, 'sha512').toString('hex');
+    };
+
+    _getUserIDFromLabel = (label) => {
+        const pos = label.indexOf('#');
+        return label.slice(pos + 1, pos + 1 + this._USERID_LENGTH);
+    };
+
+    _hidePasswordInfo = (userInfo) => {
+        return {
+            userID: this._getUserIDFromLabel(userInfo.pk),
+            email: userInfo.email,
+            userName: userInfo.userName,
+        };
+    };
+
+    register = async (email, password, userName) => {
+        console.log(`[AuthService] ${email} try to register`);
+
+        // check registered or not
+        let create;
+        let userInfo = await this._dynamodbService.getUserInfoFromEmail(email);
+
+        if (userInfo !== null) {
+            // registered
+            console.log(`[AuthService] ${email} already registered`);
+            create = false;
+        } else {
+            // not yet registered
+            console.log(`[AuthService] ${email} not registered yet`);
+            const userID = await this._genNewUserID();
+            const passwordSalt = crypto.randomBytes(16).toString('hex');
+            const passwordHash = this._hashPassword(password, passwordSalt);
+            await this._dynamodbService.createUserInfo(userID, email, passwordSalt, passwordHash, userName);
+
+            create = true;
+            userInfo = await this._dynamodbService.getUserInfoFromEmail(email);
+
+            console.log(`[AuthService] ${email} successfully registered`);
+        }
+
+        return {
+            create,
+            data: this._hidePasswordInfo(userInfo),
+        };
+    };
+
+    login = async (email, password) => {
+        // login
+        console.log(`[AuthService] ${email} try to login`);
+
+        // 1. get related user info
+        let userInfo = await this._dynamodbService.getUserInfoFromEmail(email);
+        if (userInfo === null) {
+            // not yet registered
+            console.log(`[AuthService] ${email} not yet registered`);
+            return { success: false, error: 'Not yet registered' };
+        }
+        // 2. check password
+        const passwordSalt = userInfo.passwordSalt;
+        if (this._hashPassword(password, passwordSalt) !== userInfo.passwordHash) {
+            console.log(`[AuthService] ${email} login with invalid password`);
+            return { success: false, error: 'Invalid password' };
+        }
+
+        // 3. login successfully
+        return { success: true, data: this._hidePasswordInfo(userInfo) };
+    };
+}
+
+export default AuthService;
