609427d41072aba49bb7f0d2ebe883d1e9903b29
Esther125 <friday50523@gmail.com>
2024-12-01 21:22:39 +0800

    fix: solve conflict error

--- backend/src/controllers/internetFileController.js
+++ backend/src/controllers/internetFileController.js
@@ -4,17 +4,27 @@ import path from 'path';
 import { logWithFileInfo } from '../../logger.js';
 
 class InternetFileController {
-    async upload(req, res) {
-        logWithFileInfo('info', '----InternetFileController.upload');
-        // TODO: 實現上傳檔案邏輯
-        res.status(201).json({ message: 'File upload logic not implemented yet' });
+    constructor() {
+        this.internetFileService = new InternetFileService();
     }
 
-    async download(req, res) {
-        logWithFileInfo('info', '----InternetFileController.download');
+    upload = async (req, res) => {
+        logWithFileInfo('info', '----InternetFileController.upload');
+        try {
+            const filename = await this.internetFileService.upload(req, res);
+            const fileId = path.basename(filename, path.extname(filename));
+            res.status(200).json({ message: 'File uploaded successfully.', fileId: fileId });
+        } catch (error) {
+            logWithFileInfo('info', '----InternetFileController.download');
+            res.status(500).json({ message: 'Failed to upload the file.', error: error.message });
+        }
+    };
+
+    download = async (req, res) => {
+        console.log('----InternetFileController.download');
         // TODO: 實現下載檔案邏輯
         res.status(200).json({ message: 'File download logic not implemented yet' });
-    }
+    };
 }
 
 export default InternetFileController;
