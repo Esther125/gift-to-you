abf58c1e4c947e5d75652d2426b8dd3bf7a18f73
amyhsu <amy991225@gmail.com>
2024-12-07 23:44:00 +0800

    CNS-42 feat: add redirection in frontend when no authentication

---
+++ frontend/src/api/api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_BE_API_BASE_URL,
+    withCredentials: true,
+    timeout: 5000,
+});
+
+api.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        if (error.response && error.response.status === 401) {
+            alert('未登入，重新導向');
+            window.location.href = '/login';
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default api;
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import api from '@/api/api';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,13 +17,33 @@ const router = createRouter({
             // this generates a separate chunk (About.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
             component: () => import('../views/StorageView.vue'),
+            meta: { requireAuth: true },
         },
         {
             path: '/history',
             name: 'history',
             component: () => import('../views/HistoryView.vue'),
+            meta: { requireAuth: true },
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: () => import('../views/LoginView.vue'),
         },
     ],
+});
+
+router.beforeEach(async (to, from, next) => {
+    if (to.meta.requireAuth) {
+        try {
+            await api.get('/auth-check');
+        } catch (error) {
+            if (!error.response || error.response.status !== 401) {
+                next(false);
+            }
+        }
+    }
+    next();
 });
 
 export default router;
