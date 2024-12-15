3b7beb2b6a3bba598d15f9986c43ce4bc6ad0175
Esther125 <friday50523@gmail.com>
2024-12-15 23:07:35 +0800

    CNS-56 fix: update redis client usage

--- backend/src/services/internetFileService.js
+++ backend/src/services/internetFileService.js
@@ -1,7 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import multer from 'multer';
 import fs from 'fs';
 import crypto from 'crypto';
 import S3Service from './s3Service.js';
@@ -10,8 +9,6 @@ import { logWithFileInfo } from '../../logger.js';
 
 class InternetFileService {
     constructor() {
-        this.redisClient = new redisClient();
-        this.redisClient.connect();
         this.__filename = fileURLToPath(import.meta.url); // 當前檔名
         this.__dirname = path.dirname(this.__filename); // 當前目錄名
         this.uploadPath = path.join(this.__dirname, '../../uploads');
@@ -33,6 +30,8 @@ class InternetFileService {
     // 若不存在，將檔案存入 uploads 資料夾並記錄 hash 值
     uploadFile = async (req, res, next) => {
         try {
+            await redisClient.connect();
+
             if (!req.file) {
                 throw new Error('No file was uploaded.');
             }
@@ -41,13 +40,13 @@ class InternetFileService {
             const fileHash = this._calculateFileHash(fileBuffer);
 
             // 從 Redis 檢查 hash 是否已存在
-            const cachedFile = await this.redisClient.get(fileHash);
+            const cachedFile = await redisClient.get(fileHash);
 
             let fullFilename;
             if (cachedFile) {
                 // 檔案已經存在
                 fullFilename = cachedFile;
-                logWithFileInfo('info', `File (${fullFilename}) already exists in the server.`);
+                logWithFileInfo('info', `File: ${fullFilename} already exists in the server.`);
             } else {
                 // 檔案不存在
                 fullFilename = this._generateUniqueFilename(req.file.originalname);
@@ -57,7 +56,8 @@ class InternetFileService {
                 await fs.promises.writeFile(filePath, fileBuffer);
 
                 // 把新的 hash 值存入 Redis
-                await this.redisClient.set(fileHash, fullFilename, { EX: 3600 * 24 * 30 }); // 30 天後過期
+                await redisClient.set(fileHash, fullFilename);
+                await redisClient.setExpire(fileHash, 3600 * 24 * 30); // 30 天後過期
                 logWithFileInfo('info', `File saved as ${fullFilename}`);
             }
             return fullFilename;
