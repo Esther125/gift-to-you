7c8927cc6ee6b585357fbb677baaf7a06e5bfc2a
amyhsu <amy991225@gmail.com>
2024-12-21 14:16:36 +0800

    CNS-42 fix: error after jwt token expired

--- backend/src/middlewares/jwtMiddleware.js
+++ backend/src/middlewares/jwtMiddleware.js
@@ -4,7 +4,7 @@ import { logWithFileInfo } from '../../logger.js';
 
 const authService = new AuthService();
 
-const jwtMiddleware = (req, res, next) => {
+const jwtMiddleware = async (req, res, next) => {
     logWithFileInfo('info', '----- JWT -----');
 
     const { routerQuery } = req.query;
@@ -16,31 +16,50 @@ const jwtMiddleware = (req, res, next) => {
         return res.status(401).json({ error: 'unauthorized', toPath, routerQuery });
     }
 
-    let userData;
     // check if accessToken is expired, if expired generate a new one
-    jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        userData = _extractUserDataFromJwt(user);
+    let userData;
+    try {
+        // try accessToken first
+        const userFromAccessToken = await _jwtVerify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+        userData = _extractUserDataFromJwt(userFromAccessToken);
+    } catch (err) {
+        if (err.name === 'TokenExpiredError') {
+            try {
+                // try refreshToken
+                const userFromRefreshToken = await _jwtVerify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+                userData = _extractUserDataFromJwt(userFromRefreshToken);
 
-        if (err) {
-            // accessToken expired
-            jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-                if (err) {
-                    // invalid refreshToken
-                    logWithFileInfo('info', `invalid refreshToken when go to ${toPath}`);
-                    return res.status(401).json({ error: 'unauthorized', toPath, routerQuery });
-                }
-
-                userData = _extractUserDataFromJwt(user);
+                // regenerate accessToken
                 logWithFileInfo('info', 'accessToken expired, regenerate accessToken');
                 const newAccessToken = authService.generateAccessToken(userData);
                 res.cookie('accessToken', newAccessToken, { httpOnly: true, secure: true });
-            });
+            } catch (err) {
+                if (err.name === 'TokenExpiredError') {
+                    logWithFileInfo('info', `invalid refreshToken when go to ${toPath}`);
+                    return res.status(401).json({ error: 'unauthorized', toPath, routerQuery });
+                } else {
+                    logWithFileInfo('error', 'Error in jwtMiddleware', err);
+                    return;
+                }
+            }
+        } else {
+            logWithFileInfo('error', 'Error in jwtMiddleware', err);
+            return;
         }
+    }
 
-        // add userData to req
-        req.user = userData;
-        logWithFileInfo('info', `user ${req.user.userID} pass jwtMiddleware when go to ${toPath}`);
-        next();
+    // add userData to req
+    req.user = userData;
+    logWithFileInfo('info', `user ${req.user.userID} pass jwtMiddleware when go to ${toPath}`);
+    next();
+};
+
+const _jwtVerify = async (token, secret) => {
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, secret, (err, decoded) => {
+            if (err) return reject(err);
+            resolve(decoded);
+        });
     });
 };
 
